12cbdd3cb08b82add53b14d306c87f193b40f877
FeatherWhite <tomw5823@outlook.com>
2021-02-26 18:54:50 +0800

    wpf简易文件资源管理器

---
+++ Filesystem/FileSys.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using System.ComponentModel;
+
+namespace FolderExplorer.Filesystem
+{
+    public abstract class FileSys : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, e);
+            }
+        }
+        /// <summary>
+        /// 获取一个文件或文件夹
+        /// </summary>
+        protected FileSystemInfo folderOrfile;
+        /// <summary>
+        /// 获取文件实例
+        /// </summary>
+        protected DirectoryInfo folder;
+        /// <summary>
+        /// 获取文件实例 
+        /// </summary>
+        protected FileInfo file;
+        protected string name;
+        /// <summary>
+        /// 获取文件夹或文件名称
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                if (name == null)
+                {
+                    name = folderOrfile.Name;
+                }
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Name"));
+            }
+        }
+        /// <summary>
+        /// 传入路径创建一个文件或文件夹对象
+        /// </summary>
+        public string FullPath
+        {
+            get
+            {
+                return folderOrfile.FullName;
+            }
+            set
+            {
+                if (Directory.Exists(value))
+                {
+                    //一个标记(重点)
+                    this.folder = new DirectoryInfo(value);
+                    folderOrfile = this.folder;
+                }
+                else if (System.IO.File.Exists(value))
+                {
+                    this.file = new FileInfo(value);
+                    folderOrfile = this.file;
+                }
+                else
+                {
+                    throw new ArgumentException("must exist", value);
+                }
+                OnPropertyChanged(new PropertyChangedEventArgs("FullPath"));
+            }
+        }
+
+        /// <summary>
+        /// 获取文件或文件夹修改时间
+        /// </summary>
+        private string changeTime;
+        public string ChangeTime
+        {
+            get
+            {
+                changeTime = folderOrfile.LastWriteTime.ToString();
+                return changeTime;
+            }
+        }
+        /// <summary>
+        /// 获取文件夹或文件的图标
+        /// </summary>
+        private Icon icon;
+
+        public Icon iIcon
+        {
+            get
+            {
+                if (icon == null)
+                {
+                    switch (folderOrfile is DirectoryInfo)
+                    {
+                        case true:
+                            icon = IconInfo.GetFolderIcon(false);
+                            break;
+                        case false:
+                            {
+                                var path = file.FullName;
+                                icon = IconInfo.GetFileIcon(path, true);
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                return icon;
+            }
+            set
+            {
+                icon = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("iIcon"));
+            }
+        }
+
+        /// <summary>
+        /// 获取文件类型
+        /// </summary>
+        private string fType;
+
+        public string FType
+        {
+            get
+            {
+                switch (folderOrfile is DirectoryInfo)
+                {
+                    case true:
+                        {
+                            if ((folder.Attributes & FileAttributes.Directory) == FileAttributes.Directory)
+                            {
+                                fType = "文件夹";
+                            }
+                        }
+                        break;
+                    case false:
+                        {
+                            fType = FileTypeInfo.GetFileType(file.FullName);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                return fType;
+            }
+        }
+        /// <summary>
+        /// 获取文件大小
+        /// </summary>
+        private long? size;
+
+        public long? Size
+        {
+            get 
+            {
+                if (file != null)
+                    //空格是用来保持与类型cell之间的距离
+                    size = (file.Length / 1024);
+                //文件夹的大小为空
+                else size = null;
+                return size;
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// 自定义文件夹类型
+    /// </summary>
+    public class Folder : FileSys
+    {
+        private ObservableCollection<Folder> subFolders;
+        /// <summary>
+        /// 当前文件夹下的子文件夹
+        /// </summary>
+        public ObservableCollection<Folder> SubFolders
+        {
+            get
+            {
+                if (subFolders == null)
+                {
+                    try
+                    {
+                        subFolders = new ObservableCollection<Folder>();
+                        var subFolderArray = folder.GetDirectories();
+                        foreach (var subFolder in subFolderArray)
+                        {
+                            if ((subFolder.Attributes & FileAttributes.System) != FileAttributes.System)
+                            {
+                                var newFolder = new Folder();
+                                newFolder.FullPath = subFolder.FullName;
+                                subFolders.Add(newFolder);
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                return subFolders;
+            }
+        }
+
+        private ObservableCollection<File> files;
+        /// <summary>
+        /// 当前文件夹下的文件
+        /// </summary>
+        public ObservableCollection<File> Files
+        {
+            get
+            {
+                if (files == null)
+                {
+                    try
+                    {
+                        files = new ObservableCollection<File>();
+                        FileInfo[] fileArray = folder.GetFiles();
+                        foreach (var file in fileArray)
+                        {
+                            if ((file.Attributes & FileAttributes.System) != FileAttributes.System)
+                            {
+                                var newFile = new File();
+                                newFile.FullPath = file.FullName;
+                                files.Add(newFile);
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                return files;
+            }
+        }
+
+        private ObservableCollection<FileSys> subFolderAndfiles;
+        /// <summary>
+        /// 获得子文件和文件夹的名字
+        /// </summary>
+        public ObservableCollection<FileSys> FolderAndFiles
+        {
+            get
+            {
+                //这里
+                if (subFolderAndfiles == null)
+                {
+                    try
+                    {
+                        //这里要研究一下
+                        subFolderAndfiles = new ObservableCollection<FileSys>();
+                        var folderOrfile = new Folder();
+                        folderOrfile.FullPath = folder.FullName;
+                        foreach (var subfolder in folderOrfile.SubFolders)
+                        {
+                            subFolderAndfiles.Add(subfolder);
+                        }
+                        foreach (var file in folderOrfile.Files)
+                        {
+                            subFolderAndfiles.Add(file);
+                        }
+                        return subFolderAndfiles;
+                    }
+                    catch (Exception)
+                    {
+                        throw new ArgumentException("must exist", "path");
+                    }
+                }
+                return subFolderAndfiles;
+            }
+        }
+
+
+    }
+    /*======================================================================*/
+    /// <summary>
+    /// 自定义文件类型
+    /// </summary>
+    public class File : FileSys
+    {
+        //private Icon icon;
+        ///// <summary>
+        ///// 获取文件图标的另一种方法
+        ///// </summary>
+        //public Icon iIcon
+        //{
+        //    get
+        //    {
+        //        var path = this.file.FullName;
+        //        icon = System.Drawing.Icon.ExtractAssociatedIcon(path);
+        //        return icon;
+        //    }
+        //}
+    }
+}
+
---
+++ HomePage.xaml.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.IO;
+using FolderExplorer.Filesystem;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Interop;
+using System.Diagnostics;
+using FolderExplorer.Strategy;
+using System.ComponentModel;
+
+namespace FolderExplorer
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+            InitializeTreeView(); // 初始化文件树
+                                  //InitializeHeadNode();// 初始化头节点
+            showContextMenu.Text = "1";
+            #region "显示其可视化树"
+            //VisualTreeDisplay treeDisplay = new VisualTreeDisplay();
+            //treeDisplay.ShowVisualTree(this.TestDoubleClick);
+            //treeDisplay.Show();
+            #endregion
+        }
+        /// <summary>
+        /// 初始化一个导航用的双向链表
+        /// </summary>
+        DbLink<Folder> HistoryDbLink = new DbLink<Folder>();
+        /// <summary>
+        /// 创建一个视图
+        /// </summary>
+        ListCollectionView dgrdView;
+        public void InitializeTreeView()
+        {
+
+            ObservableCollection<Folder> folders = new ObservableCollection<Folder>();
+            //List<string> paths = new List<string>();
+            //paths.Add(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos));
+            //paths.Add(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures));
+            //paths.Add(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+            //paths.Add(Environment.GetFolderPath(Environment.SpecialFolder.MyMusic));
+            //paths.Add(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+            Drive d = new Drive();
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (var drive in drives)
+            {
+                string path = drive.Name;
+                Folder newFolder = new Folder();
+                newFolder.FullPath = path;
+                newFolder.Name = d.GetDriveName(drive);
+                newFolder.iIcon = IconInfo.GetDriverIcon(path[0], false);
+                folders.Add(newFolder);
+            }
+            //foreach (var path in paths)
+            //{
+            //    Folder newFolder = new Folder();
+            //    newFolder.FullPath = path;
+            //    folders.Add(newFolder);
+            //}
+            tvwDisplayFolder.ItemsSource = folders;
+        }
+        /// <summary>
+        /// 双击打开文件夹（DataGrid控件中）
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenFileOrFolder(object sender, MouseButtonEventArgs e)
+        {
+            switch (e.ClickCount)
+            {
+                case 2:
+                    {
+                        try
+                        {
+                            FileSys currFolder = dgrd_Display.CurrentItem as FileSys;
+                            
+                            //view = (ListCollectionView)CollectionViewSource.GetDefaultView(dgrd_Display.CurrentItem);
+                            var path = currFolder.FullPath;
+                            if (currFolder is Folder)
+                            {
+                                Folder f = currFolder as Folder;
+                                HistoryDbLink.AddNode(f);
+                                #region "测试添加了几个节点"
+                                //HistoryDbLink.Count++;
+                                //nodeCount.Text = HistoryDbLink.Count.ToString();
+                                #endregion
+                                ShowFoldersOrFiles(f);
+                            }
+                            else
+                            {
+                                Process.Start(path);
+                            }
+                        }
+                        catch (Exception)
+                        {
+
+                            throw;
+                        }
+
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        /// <summary>
+        /// 单击TreeView后触发的事件
+        /// 将当前选中文件夹的子文件夹显示在DataGrid中
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DisplaySubFoldersAndFilesOnDGRD(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            Folder currFolder = tvwDisplayFolder.SelectedItem as Folder;
+            //将打开的文件夹加入节点
+            HistoryDbLink.AddNode(currFolder);
+            #region "测试添加了几个节点"
+            //HistoryDbLink.Count++;
+            //nodeCount.Text = HistoryDbLink.Count.ToString();
+            #endregion
+            ShowFoldersOrFiles(currFolder);           
+        }
+        /// <summary>
+        /// 导航，向后
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnGoBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (!HistoryDbLink.IsCurrentBeforeFirst)
+            {
+                HistoryDbLink.GoBack();
+                Folder tf = HistoryDbLink.CurrElement;
+                ShowFoldersOrFiles(tf);
+                btnGoForward.IsEnabled = true;
+                if (HistoryDbLink.IsCurrentBeforeFirst)
+                    btnGoBack.IsEnabled = false;
+            }
+        }
+        /// <summary>
+        /// 导航，向前
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnGoForward_Click(object sender, RoutedEventArgs e)
+        {
+            if (!HistoryDbLink.IsCurrentAfterLast)
+            {
+                HistoryDbLink.GoFoward();
+                Folder tf = HistoryDbLink.CurrElement;
+                ShowFoldersOrFiles(tf);
+                btnGoBack.IsEnabled = true;
+                if (HistoryDbLink.IsCurrentAfterLast)
+                    btnGoForward.IsEnabled = false;
+            }
+        }
+        /// <summary>
+        /// 单击TreeView或者双击DataGrid时显示文件夹内容
+        /// </summary>
+        /// <param name="folder"></param>
+        private void ShowFoldersOrFiles(Folder folder)
+        {
+            //dgrd_Display.Items.Clear();
+            ObservableCollection<FileSys> folderAndFiles = folder.FolderAndFiles;
+            //创建视图用来过滤排序
+            //foreach (var folderAndfile in folderAndFiles)
+            //{
+            //    dgrd_Display.Items.Add(folderAndfile);
+            //}
+            dgrd_Display.ItemsSource = folderAndFiles;
+            dgrdView = (ListCollectionView)CollectionViewSource.GetDefaultView(dgrd_Display.ItemsSource);
+            btnGoBack.IsEnabled = true;
+            //对当前文件夹进行监视
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            try
+            {
+                watcher.Path = folder.FullPath;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            watcher.NotifyFilter = NotifyFilters.LastWrite
+                | NotifyFilters.FileName
+                | NotifyFilters.DirectoryName;
+            watcher.Filter = "";
+            watcher.Created += Watcher_Created;
+            watcher.EnableRaisingEvents = true;
+        }
+        /// <summary>
+        /// 刷新GridView中的文件夹
+        /// </summary>
+        /// <param name="folderAndFiles"></param>
+        private void Refresh(ObservableCollection<FileSys> folderAndFiles)
+        {
+            dgrd_Display.Items.Clear();
+            foreach (var folderAndfile in folderAndFiles)
+            {
+                dgrd_Display.Items.Add(folderAndfile);
+            }
+        }
+
+        private void Watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            //Folder folder = new Folder();
+            //folder.FullPath = Directory.GetParent(e.FullPath).FullName;
+            //ObservableCollection<FileSys> folderAndFiles = folder.FolderAndFiles;
+            //Action<ObservableCollection<FileSys>> action = (ff) =>
+            //{
+            //    Refresh(ff);
+            //};
+            //dgrd_Display.Dispatcher.BeginInvoke(action,folderAndFiles);
+
+            Action action = () =>
+            {
+                Folder folder = new Folder();
+                folder.FullPath = e.FullPath;
+                dgrd_Display.Items.Add(folder);
+            };
+            dgrd_Display.Dispatcher.BeginInvoke(action);
+        }
+        /// <summary>
+        /// 对DataGrid进行分组
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GroupBy(object sender, RoutedEventArgs e)
+        {
+            if (dgrdView == null)
+            { return; }
+            MenuItem cm = e.Source as MenuItem;
+            if (cm == null)
+            { return; }
+            dgrdView.GroupDescriptions.Clear();
+            switch (cm.Name)
+            {
+                case "cmName":
+                    dgrdView.GroupDescriptions.Add(new PropertyGroupDescription("Name", new FileNameGrouper()));
+                    break;
+                case "cmTime":
+                    dgrdView.GroupDescriptions.Add(new PropertyGroupDescription("ChangeTime", new FileNameGrouper()));
+                    break;
+                case "cmType":
+                    dgrdView.GroupDescriptions.Add(new PropertyGroupDescription("FType", new FileNameGrouper()));
+                    break;
+                case "cmSize":
+                    {
+                        dgrdView.GroupDescriptions.Add(new PropertyGroupDescription("Size",new FileSizeGrouper()));
+                        showContextMenu.Text = "选中大小分组";
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void SortBy(object sender, RoutedEventArgs e)
+        {
+            MenuItem cm = e.Source as MenuItem;
+            if (dgrdView == null || cm == null)
+            {
+                return;
+            }
+            switch (cm.Name)
+            {
+                case "cmSortByName":
+                    dgrdView.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
+                    break;
+                case "cmSortByTime":
+                    dgrdView.SortDescriptions.Add(new SortDescription("ChangeTime", ListSortDirection.Ascending));
+                    break;
+                case "cmSortByType":
+                    dgrdView.SortDescriptions.Add(new SortDescription("FType", ListSortDirection.Ascending));
+                    break;
+                case "cmSortBySize":
+                    dgrdView.SortDescriptions.Add(new SortDescription("Size", ListSortDirection.Ascending));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    /// <summary>
+    /// 对获取到的Icon类型的图标进行转换
+    /// （DataGrid中的显示文件图标）
+    /// </summary>
+    [ValueConversion(typeof(Icon), typeof(ImageSource))]   // Icon是源类型，ImageSource是目标类型。
+    public class IconConverter : IValueConverter           //继承了 IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                Icon icon = (Icon)value;
+                Bitmap bitmap = icon.ToBitmap();
+                IntPtr hBitmap = bitmap.GetHbitmap();
+                ImageSource bitmapSource =
+                Imaging.CreateBitmapSourceFromHBitmap(
+                hBitmap, IntPtr.Zero, Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+                return bitmapSource;
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        //逆操作用不到
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
